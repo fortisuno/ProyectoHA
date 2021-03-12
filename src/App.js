cb18b1bc942be95d3588be2f56ce060166349980
Pablo Gomez <pgomezm1400@alumno.ipn.mx>
2021-03-12 12:51:29 -0600

    cambie un titulo, arregle un error, termine mi parte

--- src/App.js
+++ src/App.js
@@ -5,6 +5,16 @@ export default function App() {
   return (
     <div>
       <Title />
+      <span>Hola</span>
+      <span>Hola</span>
+      <span>Hola</span>
+      <span>Hola</span>
+      <span>Hola</span>
+      <div>
+        <div>
+          
+        </div>
+      </div>
     </div>
   )
 }
---
+++ src/utils/agregarTarea.js
@@ -0,0 +1,5 @@
+export function agregarTarea() {
+  // todo la funcion
+
+  return 'Algo'
+}
