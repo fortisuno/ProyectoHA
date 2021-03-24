edcc62dd69984b12b52b935745b7bbce085a0ccb
Pablo Gomez <pgomezm1400@alumno.ipn.mx>
2021-03-24 00:12:08 -0600

    cambios v.0.1.1

--- src/components/Pendiente.js
+++ src/components/Pendiente.js
@@ -1,14 +1,25 @@
 import React from 'react'
 
-export default function Pendiente() {
+export default function Pendiente({description}) {
   return (
-    <tr>
-      <th className="align-middle" scope="row">1</th>
-      <td className="align-middle">Herramintas automatizadas</td>
-      <td className="align-middle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Iusto, distinctio?</td>
-      <td className="align-middle text-center text-muted">26/03/2021</td>
-      <td className="align-middle text-center"><button className="btn btn-link rounded-pill py-1 px-2"><i className="bi bi-pencil-fill"></i></button></td>
-      <td className="align-middle text-center"><button className="btn btn-link text-danger rounded-pill py-1 px-2"><i className="bi bi-trash"></i></button></td>
-    </tr>
+    <div className="card shadow-sm overflow-hidden" style={{background: '#ffe97f'}}>
+      <div className="card-header border-0">
+        <div className="d-flex justify-content-between align-items-center">
+          <h5 className="mb-0">U. aprendizaje</h5>
+          <span className="fw-bold">26/03/2021</span>
+        </div>
+      </div>
+      <div className="card-body py-4">
+        <p className="lead">
+          {description}
+        </p>
+      </div>
+      <div className="card-footer border-0 bg-transparent">
+        <div className="d-flex justify-content-end align-items-center">
+          <button className="btn btn-link text-decoration-none text-dark fs-6 p-2 rounded-circle me-3" style={{background: 'rgba(0,0,0,0.08)'}}><i className="bi bi-pencil d-flex align-items-center" /></button>
+          <button className="btn btn-link text-decoration-none text-dark fs-6 p-2 rounded-circle" style={{background: 'rgba(0,0,0,0.08)'}}><i className="bi bi-trash d-flex align-items-center" /></button>
+        </div>
+      </div>
+    </div>
   )
 }
--- src/views/DashboardView.js
+++ src/views/DashboardView.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Pendiente from '../components/Pendiente'
+import Masonry, {ResponsiveMasonry} from "react-responsive-masonry"
 
 export default function DashboardView({history}) {
   
@@ -10,62 +11,53 @@ export default function DashboardView({history}) {
   return (
     <div className="bg-light">
       <div className="container">
-        <div className="row min-vh-100 w-100 py-5 align-content-start">
-          <div className="col-12">
-            <div className="d-flex shadow-sm align-items-center justify-content-between bg-primary py-3 px-4 mb-4" style={{borderRadius: '20px'}}>
-              <img src="/logoPag.png" className="d-block rounded-circle" width="48" alt=""/>
-              <button className="btn text-white rounded-pill fs-5" onClick={ handleLogout }>Salir</button>
-            </div>
+        <div className="row min-vh-100 w-100 py-5 align-items-start">          
+          <div className="col">
+            <ResponsiveMasonry>
+              <Masonry gutter="24px">
+                <Pendiente description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et posuere diam. Aliquam erat volutpat. Vestibulum quis ipsum cursus, dapibus magna non, congue purus. "/>
+                <Pendiente description="Pellentesque ultricies erat vel ultricies faucibus. Donec at venenatis lorem, at luctus orci."/>
+                <Pendiente description="Praesent vitae tristique dui. Morbi mattis risus non tristique tincidunt. Mauris volutpat mauris nec blandit suscipit. Vestibulum faucibus laoreet neque ac consectetur. Nullam eu magna ex."/>
+                <Pendiente description="Praesent vel orci est. Duis semper pulvinar leo. Fusce nec velit leo. Nullam ac ipsum sem. Nullam enim metus, lacinia sed interdum in, blandit quis risus. "/>
+                <Pendiente description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et posuere diam. Aliquam erat volutpat. Vestibulum quis ipsum cursus, dapibus magna non, congue purus. "/>
+                <Pendiente description="Pellentesque ultricies erat vel ultricies faucibus. Donec at venenatis lorem, at luctus orci."/>
+                <Pendiente description="Praesent vitae tristique dui. Morbi mattis risus non tristique tincidunt. Mauris volutpat mauris nec blandit suscipit. Vestibulum faucibus laoreet neque ac consectetur. Nullam eu magna ex."/>
+                <Pendiente description="Praesent vel orci est. Duis semper pulvinar leo. Fusce nec velit leo. Nullam ac ipsum sem. Nullam enim metus, lacinia sed interdum in, blandit quis risus. "/>
+                <Pendiente description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed et posuere diam. Aliquam erat volutpat. Vestibulum quis ipsum cursus, dapibus magna non, congue purus. "/>
+                <Pendiente description="Pellentesque ultricies erat vel ultricies faucibus. Donec at venenatis lorem, at luctus orci."/>
+                <Pendiente description="Praesent vitae tristique dui. Morbi mattis risus non tristique tincidunt. Mauris volutpat mauris nec blandit suscipit. Vestibulum faucibus laoreet neque ac consectetur. Nullam eu magna ex."/>
+                <Pendiente description="Praesent vel orci est. Duis semper pulvinar leo. Fusce nec velit leo. Nullam ac ipsum sem. Nullam enim metus, lacinia sed interdum in, blandit quis risus. "/>
+              </Masonry>
+            </ResponsiveMasonry>
           </div>
-          <div className="col-12 mb-4 position-relative">
+          <div className="col-md-3" style={{maxHeight: '100vh'}}>
             <div className="d-grid gap-4">
+              <div className="card shadow-sm">
+                <div className="card-body">
+                  <h5 className="text-center">¡Bienvenido de nuevo usuario!</h5>
+                </div>
+              </div>
               <div className="card shadow-sm">
                 <div className="card-body">
                   <h5 className="car-title mb-0">Nueva tarea</h5>
                   <hr className="mb-4 mt-2"/>
-                  <div className="row g-3">
-                    <div className="col">
-                      <input type="text" className="form-control" placeholder="Unidad de aprendizaje"/>
-                    </div>
-                    <div className="col-4">
-                      <input type="text" className="form-control" placeholder="Fecha de entrega"/>
-                    </div>
-                    <div className="col-12">
-                      <textarea className="form-control" rows="3" placeholder="Detalles de la tarea..."></textarea>
-                    </div>
-                    <div className="col text-center">
-                      <button className="btn btn-primary rounded-circle py-2 px-2 fs-4"><i className="bi bi-plus d-flex align-items-center"></i></button>
-                    </div>
+                  <div className="mb-3">
+                    <input type="text" className="form-control" placeholder="Unidad de aprendizaje"/>
+                  </div>
+                  <div className="mb-3">
+                    <input type="text" className="form-control" placeholder="Fecha de entrega"/>
+                  </div>
+                  <div className="mb-3">
+                    <textarea className="form-control" rows="3" placeholder="Detalles de la tarea..."></textarea>
+                  </div>
+                  <div className="">
+                    <button className="btn btn-primary d-block w-100">Agregar pendiente</button>
                   </div>
                 </div>
               </div>
+              <button className="btn btn-danger mt-5">Salir</button>
             </div>
           </div>
-          <div className="col">
-            <div className="card shadow-sm overflow-hidden">
-              <div className="card-body py-4">
-                <table className="table">
-                  <thead>
-                    <tr>
-                      <th scope="col">id</th>
-                      <th scope="col" className="col-3">Unidad Aprendizaje</th>
-                      <th scope="col" className="col-5">Detalles</th>
-                      <th className="text-center" scope="col">Fecha de entrega</th>
-                      <th className="text-center" scope="col">Estatus</th>
-                      <th className="text-center" scope="col">Opciones</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    <Pendiente />
-                    <Pendiente />
-                    <Pendiente />
-                    <Pendiente />
-                  </tbody>
-                </table>
-              </div>
-            </div>
-          </div>
-          
         </div>
       </div>
     </div>
